230aa8f88fff2b685e540768bb04294d75fc042c
Alexander Ringlein <alex.ringlein@gmail.com>
2016-10-02 23:56:24 -0400

    pdir init

--- kern/vmm/MPTComm/MPTComm.c
+++ kern/vmm/MPTComm/MPTComm.c
@@ -2,18 +2,37 @@
 
 #include "import.h"
 
+#define PAGESIZE    4096
+#define VM_USERLO   0x40000000
+#define VM_USERHI   0xF0000000
+#define VM_USERLO_PI    (VM_USERLO / PAGESIZE)
+#define VM_USERHI_PI    (VM_USERHI / PAGESIZE)
+
+#define NUM_PG_DIRS 1024
+#define NUM_PG_TBLS 1024
+
 /**
  * For each process from id 0 to NUM_IDS -1,
  * set the page directory entries sothat the kernel portion of the map as identity map,
  * and the rest of page directories are unmmaped.
  */
+
 void pdir_init(unsigned int mbi_adr)
 {
-    // TODO: define your local variables here.
 
     idptbl_init(mbi_adr);
 
-    // TODO
+    for (int i = 0; i < NUM_IDS; i++) {
+    	for (int j = 0; j < VM_USERLO_PI / NUM_PG_DIRS; j++) {
+    		set_pdir_entry_identity(i, j);
+    	}
+    	for (int j = VM_USERHI_PI /NUM_PG_DIRS; j < VM_USERHI_PI / NUM_PG_DIRS; j++) {
+    		set_pdir_entry_identity(i, j, 0);
+    	}
+    	for (int j = VM_USERHI_PI / NUM_PG_DIRS; j < NUM_PG_DIRS; j++) {
+    		set_pdir_entry_identity(i, j);
+    	}
+    }
 }
 
 /**
