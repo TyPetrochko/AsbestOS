928924c72dfb2e5d7137e79d66bc98658daa639f
Alexander Ringlein <ajr68@viper.zoo.cs.yale.edu>
2016-10-02 22:06:38 -0400

    first try for matop

--- kern/vmm/MPTOp/MPTOp.c
+++ kern/vmm/MPTOp/MPTOp.c
@@ -9,40 +9,48 @@
  */
 unsigned int get_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
-    // TODO
-    return 0;
+    unsigned int page_dir_entry_index = vaddr >> 22;
+    unsigned int page_table_entry_index = ((vaddr >> 12) & 0x003FF);
+    return get_ptbl_entry(proc_index, page_dir_entry_index, page_table_entry_index);
+
 }         
 
 // returns the page directory entry corresponding to the given virtual address
 unsigned int get_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
-    // TODO
-    return 0;
+     unsigned int page_dir_entry_index = vaddr >> 22;
+     return get_pdir_entry(proc_index, page_dir_entry_index);
 }
 
 // removes the page table entry for the given virtual address
 void rmv_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
-    // TODO
+    unsigned int page_dir_entry_index = vaddr >> 22;
+    unsigned int page_table_entry_index = ((vaddr >> 12) & 0x003FF);
+    rmv_ptbl_entry(proc_index, page_dir_entry_index, page_table_entry_index);
 }
 
 // removes the page directory entry for the given virtual address
 void rmv_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
-    // TODO
+    unsigned int page_dir_entry_index = vaddr >> 22;
+    rmv_pdir_entry(proc_index, page_dir_entry_index);
 }
 
 // maps the virtual address [vaddr] to the physical page # [page_index] with permission [perm]
 // you do not need to worry about the page directory entry. just map the page table entry.
 void set_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr, unsigned int page_index, unsigned int perm)
 {
-    // TODO
+    unsigned int page_dir_entry_index = vaddr >> 22;
+    unsigned int page_table_entry_index = ((vaddr >> 12) & 0x003FF);
+    set_ptbl_entry(proc_index, page_dir_entry_index, page_table_entry_index, page_index, perm);
 }
 
 // registers the mapping from [vaddr] to physical page # [page_index] in the page directory
 void set_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr, unsigned int page_index)
 {
-    // TODO
+     unsigned int page_dir_entry_index = vaddr >> 22;
+     set_pdir_entry(proc_index, page_dir_entry_index, page_index);
 }   
 
 // initializes the identity page table
