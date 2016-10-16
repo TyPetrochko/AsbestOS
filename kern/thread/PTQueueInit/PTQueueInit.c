67d7d2b62a410177f0b156884749c8541edd8230
Alexander Ringlein <alex.ringlein@gmail.com>
2016-10-16 03:12:25 -0400

    added working ptqueue init

--- kern/thread/PTQueueInit/PTQueueInit.c
+++ kern/thread/PTQueueInit/PTQueueInit.c
@@ -8,11 +8,13 @@
  */
 void tqueue_init(unsigned int mbi_addr)
 {
-  // TODO: define your local variables here.
+  unsigned int i;
 
-	tcb_init(mbi_addr);
+  tcb_init(mbi_addr);
 
-  // TODO
+  for (i = 0; i < NUM_IDS + 1; i++) {
+  	tqueue_init_at_id(i);
+  }
 }
 
 /**
@@ -23,7 +25,20 @@ void tqueue_init(unsigned int mbi_addr)
  */
 void tqueue_enqueue(unsigned int chid, unsigned int pid)
 {
-  // TODO
+  unsigned int tailPid;
+
+  tailPid = tqueue_get_tail(chid);
+  //if there is nothing in the queue
+  if (tailPid == NUM_IDS) {
+  	tqueue_set_head(chid, pid);
+  } else {
+  	//update the doubly linked list
+  	tcb_set_prev(pid, tailPid);
+  	tcb_set_next(tailPid, pid); 	
+  }
+
+  //regardless, update the tail
+  tqueue_set_tail(chid, pid);
 }
 
 /**
@@ -33,8 +48,29 @@ void tqueue_enqueue(unsigned int chid, unsigned int pid)
  */
 unsigned int tqueue_dequeue(unsigned int chid)
 {
-  // TODO
-  return 0;
+
+  unsigned int headPid;
+  unsigned int nextPid;
+
+  headPid = tqueue_get_head(chid);
+  //if the queue is not empty
+  if (headPid != NUM_IDS) {
+  	//get the 2nd pid in queue
+  	nextPid = tcb_get_next(headPid);
+  	if (nextPid != NUM_IDS) {
+  		//detach 
+  		tcb_set_prev(nextPid, NUM_IDS);
+  	} else {
+  		//now queue is empty
+  		tqueue_set_tail(chid, NUM_IDS);
+  	}
+  	//detach head
+  	tcb_set_next(headPid, NUM_IDS);
+  	//set the new head
+  	tqueue_set_head(chid, nextPid);
+  }
+  //regardless, return the head
+  return headPid;
 }
 
 /**
@@ -43,5 +79,31 @@ unsigned int tqueue_dequeue(unsigned int chid)
  */
 void tqueue_remove(unsigned int chid, unsigned int pid)
 {
-  // TODO
+  unsigned int head;
+  unsigned int tail;
+  unsigned int prev;
+  unsigned int next;
+
+  head = tqueue_get_head(chid);
+  tail = tqueue_get_tail(chid);
+  next = tcb_get_next(pid);
+  prev = tcb_get_prev(pid);
+
+  //remove self from list
+  tcb_set_next(pid, NUM_IDS);
+  tcb_set_prev(pid, NUM_IDS);
+  if (next != NUM_IDS) {
+    tcb_set_prev(next, prev);
+  }
+  if (prev != NUM_IDS) {
+    tcb_set_next(prev, next);
+  }
+  //fix head and tail if necessary
+  if (pid == head) {
+    tqueue_set_head(chid, next);
+  }
+  if (pid == tail) {
+    tqueue_set_tail(chid, prev);
+  }
+  //TODO: this assumes pid is in chid? should we check?
 }
