4c8d7b33455e3ef6702a17ed0ab9cb5faaf64f65
Tyler Petrochko <ty.petrochko@gmail.com>
2016-10-19 14:00:37 -0400

    Fixed some bugs, still hardCopy bug

--- kern/vmm/MPTFork/MPTFork.c
+++ kern/vmm/MPTFork/MPTFork.c
@@ -8,6 +8,8 @@
 #define NUM_DIR 1024
 #define NUM_TBL 1024
 
+#define USER_DIR_LO 256
+#define USER_DIR_HI 960
 
 void copyPages(unsigned int parentProcess, unsigned int childProcess) {
   unsigned int parentPDE;
@@ -16,10 +18,17 @@ void copyPages(unsigned int parentProcess, unsigned int childProcess) {
 	unsigned int newIndex;
 	unsigned int i;
 	unsigned int j;
+
+  unsigned int new_ptbl;
   //copy over pde
-	for (i = 0; i < NUM_DIR; i++) {
-		newIndex = container_alloc(childProcess);
-		set_pdir_entry(childProcess, i, newIndex);
+	for (i = USER_DIR_LO; i < USER_DIR_HI; i++) {
+		// newIndex = container_alloc(childProcess);
+    new_ptbl = alloc_ptbl(childProcess, i * 4096 * 1024);
+    
+    if(new_ptbl == 0)
+      KERN_PANIC("Could not allocate page table!\n");
+
+		// set_pdir_entry(childProcess, i, newIndex);
 		//set permissions of ptbls
 		for (j = 0; j < NUM_TBL; j++) {
 			pageTableEntry = get_ptbl_entry(parentProcess, i, j);
@@ -28,10 +37,10 @@ void copyPages(unsigned int parentProcess, unsigned int childProcess) {
 			set_ptbl_entry(childProcess, i, j, pageIndex, NEW_PERM);
 		}
 	}
-
 }
 
 void hardCopy(unsigned int pid, unsigned int vaddr) {
+  KERN_DEBUG("Hardcopying va = 0x%08x, pid = %d\n", vaddr, pid);
 	unsigned int entry;
 	unsigned int newPageIndex;
 	unsigned int oldPageIndex;
