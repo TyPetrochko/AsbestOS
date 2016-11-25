488d27f1a429e67cee78b47f320aa089f47d2005
Alexander Ringlein <alex.ringlein@gmail.com>
2016-11-25 15:57:14 -0500

    added dir_link

--- kern/fs/dir.c
+++ kern/fs/dir.c
@@ -22,15 +22,16 @@ dir_lookup(struct inode *dp, char *name, uint32_t *poff)
   uint32_t off, inum;
   struct dirent de;
   int read_value;
+  int dir_entry_size = sizeof(de);
 
   if(dp->type != T_DIR)
     KERN_PANIC("dir_lookup not DIR");
 
   //TODO
-  for (off = 0; off < dp->size; dp += sizeof(de)) {
-    read_value = inode_read(dp, (char *) de, off, dp->size);
-    if (read_value != sizeof(de)) {
-      KERN_PANIC("Bad lookup!");
+  for (off = 0; off < dp->size; dp += dir_entry_size) {
+    read_value = inode_read(dp, (char *) &de, off, dir_entry_size);
+    if (read_value != dir_entry_size) {
+      KERN_PANIC("Bad inode_read in dir_lookup");
     } else if (de.inum == 0) {
       //not allocated
       continue;
@@ -47,9 +48,39 @@ dir_lookup(struct inode *dp, char *name, uint32_t *poff)
 int
 dir_link(struct inode *dp, char *name, uint32_t inum)
 {
+  struct inode * node;
+  uint32_t * poff;
+  uint32_t off;
+  struct dirent de;
+  int read_value, write_value;
+  int dir_entry_size = sizeof(de);
   // TODO: Check that name is not present.
+  node = dir_lookup(dp, name, poff);
+  if (node) {
+    inode_put(node);
+    return -1;
+  }
 
   // TODO: Look for an empty dirent.
+  for (off = 0; off < dp->size; off += dir_entry_size) {
+    read_value = inode_read(dp, (char *) &de, off, dir_entry_size);
+    if (read_value != dir_entry_size) {
+      KERN_PANIC("Bad inode_read in dir_link");
+    } else if (de.inum == 0) {
+      break;
+    }
+  }
+
+  if (de.inum != 0) {
+    KERN_PANIC("No unallocated blocks in dir_link");
+  }
+  //found block, copy in info
+  de.inum = inum;
+  strncpy(de.name, name, DIRSIZ);
+  write_value = inode_write(dp, (char *) &de, off, dir_entry_size);
+  if (write_value != dir_entry_size) {
+    KERN_PANIC("Bad inode_write in dir_link");
+  }
   
   return 0;
 }
