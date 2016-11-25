fc6278a837e01db50d059364723cbbfc2c923fa5
Alexander Ringlein <alex.ringlein@gmail.com>
2016-11-25 15:02:11 -0500

    handle disk interrupts

--- kern/trap/TTrapHandler/TTrapHandler.c
+++ kern/trap/TTrapHandler/TTrapHandler.c
@@ -133,7 +133,13 @@ void interrupt_handler (tf_t *tf)
       case T_IRQ0 + IRQ_TIMER:
           timer_intr_handler ();
           break;
-      // TODO: handle the disk interrupts here
+      //handle the disk interrupts here
+      case T_IRQ0 + IRQ_IDE1:
+          ide_intr();
+          intr_eoi();
+          break;
+      case T_IRQ0 + IRQ_IDE2:
+          break;
       default:
           default_intr_handler ();
     }
