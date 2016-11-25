b4a72d72a6323c6628cd4b5515bc7ee03c844a84
Alexander Ringlein <alex.ringlein@gmail.com>
2016-11-25 15:37:54 -0500

    added dir_lookup

--- kern/fs/dir.c
+++ kern/fs/dir.c
@@ -21,12 +21,25 @@ dir_lookup(struct inode *dp, char *name, uint32_t *poff)
 {
   uint32_t off, inum;
   struct dirent de;
+  int read_value;
 
   if(dp->type != T_DIR)
     KERN_PANIC("dir_lookup not DIR");
 
   //TODO
-
+  for (off = 0; off < dp->size; dp += sizeof(de)) {
+    read_value = inode_read(dp, (char *) de, off, dp->size);
+    if (read_value != sizeof(de)) {
+      KERN_PANIC("Bad lookup!");
+    } else if (de.inum == 0) {
+      //not allocated
+      continue;
+    } else if (dir_namecmp(name, de.name) == 0) {
+      *poff = off;
+      inum = de.inum;
+      return inode_get(dp->dev, inum);
+    }
+  }
   return 0;
 }
 
